4f1aa6d9e8c283a3c494d6815a0d534e6af024f0
Anandita Deo -X (anadeo - ALLEGIS SERVICES INDIA PRIVATE LIMITED at Cisco) <anadeo@cisco.com>
2016-05-14 06:54:42 +0530

    added by nanna for babu

---
+++ SampleApp/js/resumeBuilder.js
@@ -0,0 +1,15 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+ 
+ $("#main").append("Anandita resume - ")
+ var firstName =  "Anan";
+ console.log(firstName);
+ 
+  var awesomeThoughts =  "I am "+firstName+" and I am awesome";
+ console.log(awesomeThoughts);
+ 
+  var funThoughts = awesomeThoughts.replace("awesome", "fun");
+  console.log(funThoughts);
+  
+  $("#main").append(funThoughts)
